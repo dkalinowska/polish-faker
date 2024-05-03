b3afe15e33c4706c5b0951c1021267aa55123c84
dkalinowska <kalinooovska@gmail.com>
2024-05-03 15:33:09 +0200

    added new yamls

--- src/main/java/random/person/Name.java
+++ src/main/java/random/person/Name.java
@@ -3,6 +3,19 @@ package random.person;
 public class Name {
 
     public Name() {
+    }
 
+    public String getFemaleFirstName() {
+        return "";
+    }
+
+    public enum Names {
+        FEMALE_FIRST_NAME("female.first_name"),
+        MALE_FIRST_NAME("male.first_name"),
+        FEMALE_LAST_NAME("female.last_name"),
+        MALE_LAST_NAME("male.last_name");
+
+        Names(String name) {
+        }
     }
 }
