845e53c591243f6e546e51c97f9612c382ef87ed
Dominika Kalinowska <kalinooovska@gmail.com>
2024-05-05 12:06:55 +0200

    minor changes

--- src/main/java/service/YamlHandler.java
+++ src/main/java/service/YamlHandler.java
@@ -3,6 +3,8 @@ package service;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -15,25 +17,34 @@ public class YamlHandler {
     }
 
     public List<String> handleYaml(String fileName, String... keys) {
-        Map<?, ?> yamlMap = getMap(fileName);
-        for (String key : keys) {
-            yamlMap = (Map<?, ?>) yamlMap.get(key);
+        Map<String, Object> yamlMap = getMap(fileName);
+        if (yamlMap == null) {
+            throw new IllegalArgumentException(String.format("File '%s' does not exist", fileName));
         }
-        if (yamlMap instanceof List<?> && ((List<?>) yamlMap).getFirst() instanceof String) {
-            return (List<String>) yamlMap;
+        for (String key : keys) {
+            Object value = yamlMap.get(key);
+            if (value instanceof Map<?, ?>) {
+                yamlMap = (Map<String, Object>) value;
+            } else {
+                throw new IllegalArgumentException(String.format("Key '%s' does not exist", key));
+            }
+        }
+        if (yamlMap.values().stream().allMatch(val -> val instanceof String)) {
+            return yamlMap.values().stream()
+                    .map(Object::toString)
+                    .collect(ArrayList::new, Collection::add, Collection::addAll);
         } else {
-            throw new IllegalArgumentException("The final object is not a list");
+            throw new IllegalArgumentException("The final object is not a list of strings");
         }
     }
 
-    private Map<?, ?> getMap(String fileName) {
+    public Map<String, Object> getMap(String fileName) {
         InputStream inputStream = this.getClass()
                 .getClassLoader()
                 .getResourceAsStream(fileName);
-        try {
-            return yaml.load(inputStream);
-        } catch (NullPointerException e) {
-            throw new NullPointerException("File does not exist");
+        if (inputStream == null) {
+            return null;
         }
+        return yaml.load(inputStream);
     }
 }
--- src/test/java/service/YamlHandlerTest.java
+++ src/test/java/service/YamlHandlerTest.java
@@ -8,6 +8,7 @@ import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,32 +25,49 @@ public class YamlHandlerTest {
     }
 
     @Test
-    public void handleYamlReturnsCorrectListWhenFileAndKeyExist() {
-        String fileName = "test.yaml";
-        String key = "testKey";
+    public void handleYamlReturnsCorrectListWhenFileAndKeysExist() {
+        String fileName = "test/java/service/test.yaml";
+        String[] keys = {"key1", "key2"};
         List<String> expectedList = Arrays.asList("value1", "value2", "value3");
-        Map<String, List<String>> yamlContent = Map.of(key, expectedList);
+        Map<String, Object> yamlContent = new HashMap<>();
+        yamlContent.put("key2", expectedList);
+        Map<String, Object> yamlContentNested = new HashMap<>();
+        yamlContentNested.put("key1", yamlContent);
 
-        Mockito.when(yamlMock.load(inputStreamMock)).thenReturn(yamlContent);
+        Mockito.when(yamlMock.load(inputStreamMock)).thenReturn(yamlContentNested);
 
-        List<String> result = yamlHandler.handleYaml(fileName, key);
+        List<String> result = yamlHandler.handleYaml(fileName, keys);
 
         Assert.assertEquals(expectedList, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void handleYamlThrowsExceptionWhenFinalObjectIsNotAList() {
+        String fileName = "test/java/service/test.yaml";
+        String[] keys = {"key1", "key2"};
+        Map<String, Object> yamlContent = new HashMap<>();
+        yamlContent.put("key2", "value");
+        Map<String, Object> yamlContentNested = new HashMap<>();
+        yamlContentNested.put("key1", yamlContent);
+
+        Mockito.when(yamlMock.load(inputStreamMock)).thenReturn(yamlContentNested);
+
+        yamlHandler.handleYaml(fileName, keys);
     }
 
     @Test(expected = NullPointerException.class)
     public void handleYamlThrowsExceptionWhenFileDoesNotExist() {
         String fileName = "nonexistent.yaml";
-        String key = "testKey";
+        String[] keys = {"testKey"};
 
-        yamlHandler.handleYaml(fileName, key);
+        yamlHandler.handleYaml(fileName, keys);
     }
 
     @Test(expected = NullPointerException.class)
     public void handleYamlThrowsExceptionWhenKeyDoesNotExist() {
         String fileName = "test.yaml";
-        String key = "nonexistentKey";
+        String[] keys = {"nonexistentKey"};
 
-        yamlHandler.handleYaml(fileName, key);
+        yamlHandler.handleYaml(fileName, keys);
     }
 }
